940accf8856648855275213a806c954ecb77350c
Alba Garza <albajarazeth@gmail.com>
2022-12-02 10:09:58 -0600

    commit

--- src/main/java/com/example/practicedemo/formAPI/controller/Controller.java
+++ src/main/java/com/example/practicedemo/formAPI/controller/Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("http://localhost:8080")
 @RequestMapping("/notes")
 @RestController
 public class Controller {
